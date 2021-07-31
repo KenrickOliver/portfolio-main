f0986a7c503eb1fee68789d0d5c83ad476b3f0c5
KenrickOliver <kenrick.oliver@gmail.com>
2021-07-31 17:14:20 -0400

    added footer and changed nav styling a bit

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,6 @@ navLink.forEach(nav => nav.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
-
 /*==================== QUALIFICATION TABS ====================*/
 
 
@@ -46,10 +45,38 @@ navLink.forEach(nav => nav.addEventListener('click', linkAction))
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+console.log(sections)
 
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+        console.log(sectionId);
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+            
+        } else {
+        
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
 
